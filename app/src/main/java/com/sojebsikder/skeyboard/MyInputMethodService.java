ff9d0df74305cc4fac07ecce46471028f9749936
Sojeb Sikder <sojebsikder@gmail.com>
2021-10-15 18:34:40 +0600

    main

--- app/src/main/java/com/sojebsikder/skeyboard/MyInputMethodService.java
+++ app/src/main/java/com/sojebsikder/skeyboard/MyInputMethodService.java
@@ -3,20 +3,27 @@ package com.sojebsikder.skeyboard;
 import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
+import android.media.AudioManager;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 
 public class MyInputMethodService extends InputMethodService implements KeyboardView.OnKeyboardActionListener {
 
+    private KeyboardView kv;
+    private Keyboard keyboard;
+
+    private boolean caps = false;
+
     @Override
     public View onCreateInputView() {
         // get the KeyboardView and add our Keyboard layout to it
-        KeyboardView keyboardView = (KeyboardView) getLayoutInflater().inflate(R.layout.keyboard_view, null);
-        Keyboard keyboard = new Keyboard(this, R.xml.number_pad);
-        keyboardView.setKeyboard(keyboard);
-        keyboardView.setOnKeyboardActionListener(this);
-        return keyboardView;
+        kv = (KeyboardView)getLayoutInflater().inflate(R.layout.keyboard_view, null);
+        keyboard = new Keyboard(this, R.xml.number_pad);
+        kv.setKeyboard(keyboard);
+        kv.setOnKeyboardActionListener(this);
+        return kv;
     }
 
     @Override
@@ -30,23 +37,44 @@ public class MyInputMethodService extends InputMethodService implements Keyboard
     }
 
     @Override
-    public void onKey(int i, int[] ints) {
+    public void onKey(int primaryCode, int[] keyCodes) {
         InputConnection ic = getCurrentInputConnection();
-        if (ic == null) return;
-        switch (i) {
-            case Keyboard.KEYCODE_DELETE:
-                CharSequence selectedText = ic.getSelectedText(0);
-                if (TextUtils.isEmpty(selectedText)) {
-                    // no selection, so delete previous character
-                    ic.deleteSurroundingText(1, 0);
-                } else {
-                    // delete the selection
-                    ic.commitText("", 1);
-                }
+        playClick(primaryCode);
+        switch(primaryCode){
+            case Keyboard.KEYCODE_DELETE :
+                ic.deleteSurroundingText(1, 0);
+                break;
+            case Keyboard.KEYCODE_SHIFT:
+                caps = !caps;
+                keyboard.setShifted(caps);
+                kv.invalidateAllKeys();
+                break;
+            case Keyboard.KEYCODE_DONE:
+                ic.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_ENTER));
                 break;
             default:
-                char code = (char) i;
-                ic.commitText(String.valueOf(code), 1);
+                char code = (char)primaryCode;
+                if(Character.isLetter(code) && caps){
+                    code = Character.toUpperCase(code);
+                }
+                ic.commitText(String.valueOf(code),1);
+        }
+    }
+
+    private void playClick(int keyCode){
+        AudioManager am = (AudioManager)getSystemService(AUDIO_SERVICE);
+        switch(keyCode){
+            case 32:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_SPACEBAR);
+                break;
+            case Keyboard.KEYCODE_DONE:
+            case 10:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_RETURN);
+                break;
+            case Keyboard.KEYCODE_DELETE:
+                am.playSoundEffect(AudioManager.FX_KEYPRESS_DELETE);
+                break;
+            default: am.playSoundEffect(AudioManager.FX_KEYPRESS_STANDARD);
         }
     }
 
